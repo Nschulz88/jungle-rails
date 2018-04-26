053d0de76ae13f3df109ae1f7b1699642333cb01
Nschulz88 <natalie.schulz@me.com>
2018-04-26 18:21:02 +0000

    Implemented user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,18 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  # These routes will be for signup. The first renders a form in the browse, the second will 
+  # receive the form and create a user in our database using the data given to us by the user.
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  # Could this be: ???????????????????????
+  # resources :users, only: [:create, :new]
+
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
